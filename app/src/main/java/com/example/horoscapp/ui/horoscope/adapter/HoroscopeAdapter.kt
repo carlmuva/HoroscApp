be78952fe8c5b4f3529c0eb2aae45168b19dd275
Carlos Murillo <carlmuva@hotmail.com>
2023-10-05 03:57:13 -0600

    Preparacion de horoscope detail

--- app/src/main/java/com/example/horoscapp/ui/horoscope/adapter/HoroscopeAdapter.kt
+++ app/src/main/java/com/example/horoscapp/ui/horoscope/adapter/HoroscopeAdapter.kt
@@ -6,7 +6,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.horoscapp.R
 import com.example.horoscapp.domain.model.HoroscopeInfo
 
-class HoroscopeAdapter(private var horoscopeList: List<HoroscopeInfo> = emptyList()):RecyclerView.Adapter<HoroscopeViewHolder>() {
+class HoroscopeAdapter(private var horoscopeList: List<HoroscopeInfo> = emptyList(),
+    private val onItemSelected:(HoroscopeInfo)->Unit):
+    RecyclerView.Adapter<HoroscopeViewHolder>() {
 
     fun updateList(list:List<HoroscopeInfo>){
        horoscopeList = list
@@ -21,6 +23,6 @@ class HoroscopeAdapter(private var horoscopeList: List<HoroscopeInfo> = emptyLis
     override fun getItemCount() = horoscopeList.size
 
     override fun onBindViewHolder(holder: HoroscopeViewHolder, position: Int) {
-        holder.render(horoscopeList[position])
+        holder.render(horoscopeList[position], onItemSelected)
     }
 }
